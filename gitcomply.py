4069a30f9bd7174e236466c655f26f2a0ff814ee
David Miller <il.livid.dream@gmail.com>
2009-09-19 13:30:19 +0100

    Searches recursively for git repos

--- gitcomply.py
+++ gitcomply.py
@@ -12,25 +12,56 @@ import argparse
 class GitComply:
     "Checks for files etc"
 
-    def __init__(self):
+    def search(self):
+        """Searches for git repos"""
+        if self.args.recursive:
+            for root, dirs, files in os.walk( self.dir ):
+                for dir in dirs:
+                    if dir == '.git':
+                        self.repos.append( root )
+            return self.repos
+        
+        else:
+            if os.path.exists( '.git' ):
+                self.repos.append( self.dir )
+            return self.repos
+
+
+    def __init__( self, args ):
         config = ConfigParser.RawConfigParser()
         if args.configfile:
             config_location = args.configfile
         else:
-            config_location = os.getenv('HOME')
-        configfile = os.path.join(config_location,'.gitcomply')
+            config_location = os.getenv( 'HOME' )
+        configfile = os.path.join( config_location,'.gitcomply' )
         config.read(configfile)
-        self.config = {'email': config.get('admin', 'email')}
+        self.config = { 'email': config.get( 'admin', 'email' ) }
+        self.args = args
+        self.repos = []
+        if self.args.directory:
+            self.dir = os.getcwd()
+        else:
+            self.dir = os.getcwd()
 
 
 if __name__ == '__main__':
     package_description = """Check for uncommitted files in git repos"""
-    parser = argparse.ArgumentParser(description=package_description)
-    parser.add_argument('-c', '--configfile',
-                        help="override default .gitcomply location")
+    parser = argparse.ArgumentParser( description=package_description )
+    parser.add_argument( '-c', '--configfile',
+                        help="override default .gitcomply location" )
+    parser.add_argument( '-d', '--directory',
+                         help="directory in which to begin search" )
+    parser.add_argument( '-r', '--recursive',
+                         action='store_true',
+                         help="check in all repos recursively" )
     args = parser.parse_args()
 
     # Initialize class
-    gitcomply = GitComply()
+    gitcomply = GitComply( args )
 
+    # Search for repos
+    gitcomply.search()
+    
     print gitcomply.config['email']
+    for repo in gitcomply.repos:
+        print repo
