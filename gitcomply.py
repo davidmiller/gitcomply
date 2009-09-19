a8089a342d17e46546e28c42e8a43e62ed5efceb
David Miller <il.livid.dream@gmail.com>
2009-09-19 12:58:18 +0100

    Parses config file for email, commandline arg for overriding config location

--- gitcomply.py
+++ gitcomply.py
@@ -9,3 +9,28 @@ import os
 import unittest
 import argparse
 
+class GitComply:
+    "Checks for files etc"
+
+    def __init__(self):
+        config = ConfigParser.RawConfigParser()
+        if args.configfile:
+            config_location = args.configfile
+        else:
+            config_location = os.getenv('HOME')
+        configfile = os.path.join(config_location,'.gitcomply')
+        config.read(configfile)
+        self.config = {'email': config.get('admin', 'email')}
+
+
+if __name__ == '__main__':
+    package_description = """Check for uncommitted files in git repos"""
+    parser = argparse.ArgumentParser(description=package_description)
+    parser.add_argument('-c', '--configfile',
+                        help="override default .gitcomply location")
+    args = parser.parse_args()
+
+    # Initialize class
+    gitcomply = GitComply()
+
+    print gitcomply.config['email']
